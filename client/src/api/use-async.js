909ff2bfb44bffac7199ad08672267c7320b3f18
halimyusuf <haleemyoosuph@gmail.com>
2022-08-30 01:48:20 +0100

    chore: simplified useAsync hook

--- client/src/api/use-async.js
+++ client/src/api/use-async.js
@@ -1,85 +1,41 @@
-import {
-  useCallback,
-  useLayoutEffect,
-  useEffect,
-  useRef,
-  useReducer,
-} from "react";
-
-const canUseDOM = typeof window !== "undefined";
-const useIsomorphicLayoutEffect = canUseDOM ? useLayoutEffect : useEffect;
-
-function useSafeDispatch(dispatch) {
-  const mounted = useRef(false);
-
-  useIsomorphicLayoutEffect(() => {
-    mounted.current = true;
-    return () => {
-      mounted.current = false;
-    };
-  }, []);
-
-  return useCallback(
-    (...args) => (mounted.current ? dispatch(...args) : void 0),
-    [dispatch]
-  );
-}
-
-function asyncReducer(state, action) {
-  switch (action.type) {
-    case "pending": {
-      return { status: "pending", data: null, error: null };
-    }
-    case "resolved": {
-      return { status: "resolved", data: action.data, error: null };
-    }
-    case "error": {
-      return { status: "error", data: null, error: action.error };
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`);
-    }
-  }
-}
+import { useCallback, useState } from "react";
 
 // handles asynchronous calls
 function useAsync(initialState) {
-  const [state, unsafeDispatch] = useReducer(asyncReducer, {
+  const [state, setState] = useState({
     status: "idle",
     data: null,
     error: null,
     ...initialState,
   });
 
-  const dispatch = useSafeDispatch(unsafeDispatch);
-
   const { data, error, status } = state;
 
   const run = useCallback(
     (promise) => {
-      dispatch({ type: "pending" });
+      setState({ data: null, error: null, status: "pending" });
       if (!promise) {
-        dispatch({ type: "resolved" });
+        setState({ data: null, error: null, status: "resolved" });
       }
       promise?.then(
         (data) => {
-          dispatch({ type: "resolved", data });
+          setState({ status: "resolved", data, error: null });
         },
         (error) => {
-          dispatch({ type: "error", error });
+          setState({ data: null, status: "error", error });
         }
       );
     },
-    [dispatch]
+    [setState]
   );
 
   const setData = useCallback(
-    (data) => dispatch({ type: "resolved", data }),
-    [dispatch]
+    (data) => setState({ status: "resolved", data, error: null }),
+    [setState]
   );
   const setError = useCallback(
-    (error) => dispatch({ type: "error", error }),
-    [dispatch]
+    (error) => setState({ status: "error", error, data: null }),
+    [setState]
   );
 
   return {
